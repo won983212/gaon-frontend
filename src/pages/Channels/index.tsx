8db77acf4e465b8c1b2e0b0866e8d4ee9ced0608
Sleeeeeepy <35634667+Sleeeeeepy@users.noreply.github.com>
2022-04-27 20:17:14 +0900

    Temporary pages for midterm presentation

--- src/pages/Channels/index.tsx
+++ src/pages/Channels/index.tsx
@@ -1,14 +1,37 @@
+import { ChatAvatar, ChatBorder, ChatPlainText } from '@/components/ChatItem';
+import { ChatList } from '@/components/ChatItem/style';
 import Workspace from '@/layouts/Workspace';
 import React from 'react';
 import { useParams } from 'react-router';
+import gravatar from 'gravatar';
+import styled from 'styled-components';
 
 function Channels() {
     const { channelId } = useParams();
-    return (
-        <Workspace>
-            <p>Hello</p>
+    const avatar = gravatar.url("Jo", {
+        s: '36px',
+        d: 'retro'
+    })
+    // 발표가 급하니 일단 채널에 임시로..
+    return (<Workspace>
+            <ChatPadding>
+            <ChatList>
+                <ChatAvatar src={avatar} userName="Jo" date="2022-04-25"></ChatAvatar>
+                <ChatPlainText content="안녕하세요."></ChatPlainText>
+                <ChatPlainText content="채팅 테스트입니다."></ChatPlainText>
+                <ChatPlainText content="간격이 잘 유지되는 모습이 보입니다."></ChatPlainText>
+                <ChatPlainText content="다음은 날짜 구분선입니다."></ChatPlainText>
+                <ChatBorder date="2022-04-25"></ChatBorder>
+                <ChatAvatar src={avatar} userName="Jo" date="2022-04-25"></ChatAvatar>
+                <ChatPlainText content="안녕히 계세요~"></ChatPlainText>           
+            </ChatList></ChatPadding>
         </Workspace>
+        
+
     );
 }
+const ChatPadding = styled.div`
+    padding: 2em;
+`;
 
 export default Channels;
