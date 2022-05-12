d8db4b1348bbc6fb750eb69b5ffd0633ebd9b055
이현 <won983212@naver.com>
2022-05-12 13:39:37 +0900

    chore: apiconfig 및 proxy 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const config = require('./apiconfig.json');
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     publicDir: 'public',
-    server: {
-        proxy: {
-            '/api': config.url
-        }
-    },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
