e397eaaeeb339f6efb8958525ed69164b1236335
이현 <won983212@naver.com>
2022-05-30 16:47:51 +0900

    fix: 컴파일 에러 수정

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -3,15 +3,16 @@ import { useChannelInfoSWR } from '@/api/workspace';
 
 /**
  * 현재 들어가있는 workspace / channel 정보 얻어오기
+ * channelId, workspaceId는 undefined면 -1을 기본값으로 가짐
  */
 export default function useRoom() {
     const { channelId, workspaceId } = useParams();
     const { data: channelInfo } = useChannelInfoSWR(
-        channelId === undefined ? 0 : +channelId
+        channelId === undefined ? -1 : +channelId
     );
 
     return {
         channelInfo,
-        workspaceId
+        workspaceId: workspaceId === undefined ? -1 : +workspaceId
     };
 }
