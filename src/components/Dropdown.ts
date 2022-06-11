1949111e7b5a51ac03c488baee4e71927f3e473f
Sleeeeeepy <35634667+Sleeeeeepy@users.noreply.github.com>
2022-06-12 01:08:56 +0900

    feat: Dropdown 메뉴 추가

---
+++ src/components/Dropdown.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const DropdownContent = styled.div`
+    position: absolute;
+    background-color: var(--primary);
+    min-width: 200px;
+    width: inherit;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.3);
+    z-index: 1;
+    margin-top: 64px;
+`;
+
+export const DropdownList = styled.ul`
+    list-style: none;
+`;
+
+export const DropdownItem = styled.li`
+    font-size: 15px;
+    margin-bottom: 5px;
+    align-content: center;
+    &:hover {
+        background-color: var(--primary-light);
+    }
+`;
